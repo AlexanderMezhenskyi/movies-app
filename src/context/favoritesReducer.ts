ace2fff25b7899f4f8697005b66ffddc4b8b0597
Oleksandr Mezhenskyi <oleksandr.mezhenskyi@chisw.com>
2025-04-19 01:22:53 +0200

    fix: linting

--- src/context/favoritesReducer.ts
+++ src/context/favoritesReducer.ts
@@ -2,11 +2,11 @@ import { FavoritesAction, Movie } from 'src/types/types.js';
 
 export default function favoritesReducer(state: Movie[], action: FavoritesAction): Movie[] {
   switch (action.type) {
-    case 'ADD':
-      return [...state, action.movie];
-    case 'REMOVE':
-      return state.filter((movie) => movie.id !== action.id);
-    default:
-      return state;
+  case 'ADD':
+    return [...state, action.movie];
+  case 'REMOVE':
+    return state.filter((movie) => movie.id !== action.id);
+  default:
+    return state;
   }
 }
