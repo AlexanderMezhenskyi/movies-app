95c9bf6cdc836c0e1725f37c9e4fd690cce5c977
Oleksandr Mezhenskyi <alexmezhenskyi@gmail.com>
2025-05-13 10:47:23 +0200

    chore: update project

--- src/hooks/useFavorites.ts
+++ src/hooks/useFavorites.ts
@@ -2,19 +2,19 @@ import { useContext } from 'react'
 import { FavoritesContext } from 'src/context/FavoritesContext'
 
 /**
- * Custom hook for accessing the favorites context.
+ * Custom hook for accessing the favorites' context.
  * This hook provides access to the user's list of favorite movies
  * and the dispatch function to modify the favorites state.
  * It ensures that the hook is used within a `FavoritesProvider` context.
  *
  * @returns The context value containing the `favorites` list and the `dispatch` function.
- * @throws Error if used outside of the `FavoritesProvider`.
+ * @throws Error if used outside the `FavoritesProvider`.
  */
 export const useFavorites = () => {
   // Access the context value provided by FavoritesContext
   const context = useContext(FavoritesContext)
 
-  // If context is not found, it means the hook is being used outside of FavoritesProvider
+  // If context is not found, it means the hook is being used outside FavoritesProvider
   if (!context) {
     throw new Error('useFavorites must be used within a FavoritesProvider')
   }
